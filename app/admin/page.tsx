992a6f331828b75b52d5e4e4624d8b5ccef3e61e
mehdiboudjemaa <93932483+mehdiboudjemaa@users.noreply.github.com>
2024-10-23 23:28:46 +0200

    build ok pour test vercel

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -8,7 +8,7 @@ const user = {
     name: 'Tom Cook',
     email: 'tom@example.com',
     imageUrl:
-      'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      '/photo-profile.avif',
   }
   const navigation = [
     { name: 'Add Knowledge', href: '#', current: true },
@@ -23,7 +23,7 @@ const user = {
     { name: 'Sign out', href: '#' },
   ]
 
-  function classNames(...classes) {
+  function classNames(...classes: (string | undefined)[]) {
     return classes.filter(Boolean).join(' ')
   }
 
@@ -87,7 +87,7 @@ export default function AdminPage() {
                           <MenuButton className="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                             <span className="absolute -inset-1.5" />
                             <span className="sr-only">Open user menu</span>
-                            <img alt="" src={user.imageUrl} className="h-8 w-8 rounded-full" />
+                            <Image alt="" src={user.imageUrl} className="h-8 w-8 rounded-full" width={200} height={200} />
                           </MenuButton>
                         </div>
                         <MenuItems
@@ -140,7 +140,7 @@ export default function AdminPage() {
                 <div className="border-t border-gray-700 pb-3 pt-4">
                   <div className="flex items-center px-5">
                     <div className="flex-shrink-0">
-                      <img alt="" src={user.imageUrl} className="h-10 w-10 rounded-full" />
+                      <Image alt="" src={user.imageUrl} className="h-10 w-10 rounded-full" width={200} height={200} />
                     </div>
                     <div className="ml-3">
                       <div className="text-base font-medium leading-none text-white">{user.name}</div>
